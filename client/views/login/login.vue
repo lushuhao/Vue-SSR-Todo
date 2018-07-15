2395937be52cfd9e96e219de5df881f9d0849e29
lushuhao <shahramlu@163.com>
2018-07-15 22:46:40 +0800

    新建Router配置

---
+++ client/views/login/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/sass">
+  .login {
+
+  }
+</style>
